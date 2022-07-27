589b3dd5c7e849ff1407b6af8547e7c2f2f9b9e4
Yuri Rampazo <ymrampazo@gmail.com>
2022-07-27 01:10:55 -0300

    Classe tabuleiro parcialmente implementada.

--- src/main/java/model/Tabuleiro.java
+++ src/main/java/model/Tabuleiro.java
@@ -1,4 +1,61 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+  private int linhas;
+  private int colunas;
+  private int minas;
+
+  private final List<Campo> CAMPOS = new ArrayList<>();
+
+  public Tabuleiro(int linhas, int colunas, int minas) {
+    this.linhas = linhas;
+    this.colunas = colunas;
+    this.minas = minas;
+
+    gerarCampos();
+    associarOsVizinhos();
+    sortearAsMinas();
+  }
+  private void gerarCampos() {
+    for (int i = 0; i < linhas; i++) {
+      for (int j = 0; j < colunas; j++) {
+        CAMPOS.add(new Campo(i, j));
+      }
+    }
+  }
+
+  private void associarOsVizinhos() {
+    for (Campo c1 : CAMPOS) {
+      for (Campo c2 : CAMPOS) {
+        c1.adicionarVizinho(c2);
+      }
+    }
+  }
+
+  private void sortearAsMinas() {
+    long minasArmadas = 0;
+
+    do {
+      minasArmadas = CAMPOS.stream().filter(Campo::isMinado).count();
+      int aleatorio = (int) (Math.random() * CAMPOS.size());
+      CAMPOS.get(aleatorio).setMinado(true);
+    } while (minasArmadas < minas);
+  }
+
+  public boolean objetivoAlcancado() {
+    return CAMPOS.stream().allMatch(Campo::objetivoAlcancado);
+  }
+
+  public void reiniciar() {
+    CAMPOS.forEach(Campo::reiniciar);
+    sortearAsMinas();
+  }
+
+  public String toString() {
+    return "";
+  }
 }
