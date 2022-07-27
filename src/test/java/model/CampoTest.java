82f254c88ac0244615640544fc3a3709bc6db75a
Yuri Rampazo <ymrampazo@gmail.com>
2022-07-27 00:24:09 -0300

    Testes para campos vizinhos minados incluidos

--- src/test/java/model/CampoTest.java
+++ src/test/java/model/CampoTest.java
@@ -25,6 +25,15 @@ class CampoTest {
 
     assertTrue(resultado, "Deve ser verdadeiro");
   }
+
+  @Test
+  void testAdicionarVizinho2() {
+    Campo vizinho = new Campo(2, 3);
+
+    boolean resultado = campo.adicionarVizinho(vizinho);
+
+    assertTrue(resultado, "Deve ser verdadeiro");
+  }
   @Test
   void testAdicionarVizinhoFalse() {
 
@@ -32,7 +41,17 @@ class CampoTest {
 
     boolean resultado = campo.adicionarVizinho(vizinho);
 
-    assertFalse(resultado, "Deve ser verdadeiro");
+    assertFalse(resultado, "Deve ser falso");
+  }
+
+  @Test
+  void testAdicionarVizinhoDiagonal() {
+
+    Campo vizinho = new Campo(2, 2);
+
+    boolean resultado = campo.adicionarVizinho(vizinho);
+
+    assertTrue(resultado, "Deve ser verdadeiro");
   }
 
   @Test
@@ -57,5 +76,31 @@ class CampoTest {
     campo.setMinado(true);
     assertThrows(ExplosaoException.class, () -> campo.abrir(),
           "Deve jogar a exceção personalizada ExplosãoException.");
+  }
+
+  @Test
+  void abrirComVizinhos() {
+    Campo vizinho1 = new Campo(2, 2);
+    Campo vizinho2 = new Campo(1, 1);
+
+    vizinho1.adicionarVizinho(vizinho2);
+    campo.adicionarVizinho(vizinho1);
+    campo.abrir();
+
+    assertTrue(vizinho2.isAberto()
+          && vizinho1.isAberto(), "Deve ser verdadeiro");
+  }
+
+  @Test
+  void abrirComVizinhosMinados() {
+    Campo vizinho1 = new Campo(2, 2);
+    Campo vizinho2 = new Campo(1, 1);
+    vizinho2.setMinado(true);
+
+    vizinho1.adicionarVizinho(vizinho2);
+    campo.adicionarVizinho(vizinho1);
+    campo.abrir();
+
+    assertTrue(vizinho1.isAberto() && !vizinho2.isAberto());
   }
 }
